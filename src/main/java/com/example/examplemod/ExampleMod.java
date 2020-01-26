d9c381bd8918ae3b1ac19e63c3e870f930c1bcaa
KittyCatKittyCat <elektroplaygames@gmail.com>
2020-01-26 19:57:53 +0100

    Example content

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,7 +1,12 @@
 package com.example.examplemod;
 
+import com.google.common.base.Preconditions;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -13,72 +18,71 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.IForgeRegistryEntry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.annotation.Nonnull;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("examplemod")
 public class ExampleMod
 {
-    // Directly reference a log4j logger.
+    public static String MODID = "examplemod";
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void setup(final FMLCommonSetupEvent event)
+    public ExampleMod()
     {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-    }
-
-    private void doClientStuff(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
-    }
-
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
-    }
-
-    private void processIMC(final InterModProcessEvent event)
-    {
-        // some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.getMessageSupplier().get()).
-                collect(Collectors.toList()));
-    }
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("HELLO from server starting");
     }
 
     // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
     // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents
+    {
+
+        public static Block example_block;
         @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
+        public static void registerBlocks(final RegistryEvent.Register<Block> blockRegistryEvent)
+        {
+            IForgeRegistry<Block> registry = blockRegistryEvent.getRegistry();
+
+            example_block = registerBlock(registry, new Block(Block.Properties.from(Blocks.GLASS)), "example_block");
+        }
+
+        private static <T extends Block> T registerBlock(IForgeRegistry<Block> registry, T newBlock, String name)
+        {
+            String prefixedName = MODID + ":" + name;
+            newBlock.setRegistryName(prefixedName);
+            registry.register(newBlock);
+            return newBlock;
+        }
+
+        public static Item example_item;
+        @SubscribeEvent
+        public static void registerItems(final RegistryEvent.Register<Item> itemRegistryEvent)
+        {
+            final IForgeRegistry<Item> registry = itemRegistryEvent.getRegistry();
+            registry.registerAll(
+                    example_item = setup(new BlockItem(example_block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)), "example_item")
+            );
+        }
+        @Nonnull
+        private static <T extends IForgeRegistryEntry<T>> T setup(@Nonnull final T entry, @Nonnull final String name)
+        {
+            Preconditions.checkNotNull(name, "Name to assign to entry cannot be null!");
+            return setup(entry, new ResourceLocation(MODID, name));
+        }
+
+        @Nonnull
+        private static <T extends IForgeRegistryEntry<T>> T setup(@Nonnull final T entry, @Nonnull final ResourceLocation registryName)
+        {
+            Preconditions.checkNotNull(entry, "Entry cannot be null!");
+            Preconditions.checkNotNull(registryName, "Registry name to assign to entry cannot be null!");
+            entry.setRegistryName(registryName);
+            return entry;
         }
     }
 }
